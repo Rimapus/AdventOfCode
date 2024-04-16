92f7a1ad7121505860cf8f3f1b11f810c18039a1
Wumpus_js <58144397+Rimapus@users.noreply.github.com>
2024-04-16 11:28:30 +0200

    2019 Day 7 sketch

--- 2019/Day 05/index.js
+++ 2019/Day 05/index.js
@@ -1,4 +1,5 @@
 const fs = require("fs")
+const { runComputer } = require("../Day 05/computer.js")
 let input = fs.readFileSync(`${__dirname}/input.txt`, "utf8")
 
 input = input.split(",")
@@ -7,81 +8,9 @@ input = input.split(",")
 
 console.log("AoC 2019 Day 5:")
 
-let memory = JSON.parse(JSON.stringify(input))
-
-const handleParamMode = (mode, val) => {
-    if (!mode || mode === "0") return memory[val]
-    else return val
-}
-
-function runComputer(computerInput) {
-    let computerOutput
-
-    let finished = false
-    let i = 0
-    while (!finished) {
-        const parametersMode = memory[i].toString().split("").reverse().slice(2)
-        let params = [...Array(3)].map((_, idx) => handleParamMode(parametersMode[idx], memory[i + idx + 1]))
-
-        switch (+memory[i].toString().slice(-2)) {
-            case 1:
-                memory[memory[i + 3]] = params[0] + params[1]
-                i += 4
-                break;
-
-            case 2:
-                memory[memory[i + 3]] = params[0] * params[1]
-                i += 4
-                break;
-
-            case 3:
-                memory[memory[i + 1]] = computerInput
-                i += 2
-                break;
-
-            case 4:
-                computerOutput = params[0]
-                i += 2
-                break;
-
-            case 5:
-                if (params[0] !== 0) {
-                    i = params[1]
-                } else i += 3
-                break;
-
-            case 6:
-                if (params[0] === 0) {
-                    i = params[1]
-                } else i += 3
-                break;
-
-            case 7:
-                if (params[0] < params[1]) {
-                    memory[memory[i + 3]] = 1
-                } else memory[memory[i + 3]] = 0
-                i += 4
-                break;
-
-            case 8:
-                if (params[0] === params[1]) {
-                    memory[memory[i + 3]] = 1
-                } else memory[memory[i + 3]] = 0
-                i += 4
-                break;
-
-            case 99:
-                finished = true
-                break;
-        }
-    }
-    memory = JSON.parse(JSON.stringify(input))
-    return computerOutput
-}
-
 // Part 1
-console.log(`Part 1 answer: ${runComputer(1)}`) // 7265618
+console.log(`Part 1 answer: ${runComputer([1], input)}`) // 7265618
 
 
 // Part 2
-console.log(`Part 2 answer: ${runComputer(5)}`) // 7731427
+console.log(`Part 2 answer: ${runComputer([5], input)}`) // 7731427
--- 2019/Day 07/index.js
+++ 2019/Day 07/index.js
@@ -1,12 +1,73 @@
 const fs = require("fs")
+const { runComputer } = require("../Day 05/computer.js")
 let input = fs.readFileSync(`${__dirname}/input.txt`, "utf8")
+
+input = input.split(",")
+    .map(Number)
 
 
 console.log("AoC 2019 Day 7:")
 
 // Part 1
-console.log(`Part 1 answer: ${""}`) // 
+// https://stackoverflow.com/a/9960925
+var permArr = [], usedChars = []
+function getPermutations(arr) {
+    var i, ch;
+
+    for (i = 0; i < arr.length; i++) {
+        ch = arr.splice(i, 1)[0]
+        usedChars.push(ch)
+        if (arr.length == 0) {
+            permArr.push(usedChars.slice())
+        }
+        getPermutations(arr)
+        arr.splice(i, 0, ch)
+        usedChars.pop()
+    }
+    return permArr
+};
+
+
+// let maxOutputSignal = 0
+// let phaseSettingSequences = getPermutations([0, 1, 2, 3, 4])
+
+// phaseSettingSequences.forEach(seq => {
+//     let memory = JSON.parse(JSON.stringify(input))
+//     let signal = 0
+
+//     for (let i = 0; i < 5; i++) {
+//         signal = runComputer([seq[i], signal], memory)
+//     }
+
+//     maxOutputSignal = Math.max(signal, maxOutputSignal)
+
+// })
+
+console.log(`Part 1 answer: ${maxOutputSignal}`) // 75228
 
 
 // Part 2
+
+function runBigComputer(phaseSetSeq, input = 0) {
+    let maxOutputSignal = 0
+
+
+    
+}
+
+
+// UNFINISHED
+let phaseSettingSequences = getPermutations([0, 1, 2, 3, 4])
+
+phaseSettingSequences.forEach(seq => {
+    let memory = JSON.parse(JSON.stringify(input))
+    let signal = 0
+
+    for (let i = 0; i < 5; i++) {
+        signal = runComputer([seq[i], signal], memory)
+    }
+
+    maxOutputSignal = Math.max(signal, maxOutputSignal)
+
+})
 console.log(`Part 2 answer: ${""}`) // 
