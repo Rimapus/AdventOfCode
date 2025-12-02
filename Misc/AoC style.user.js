df934a6dbaae020a98b319b5d794d50402015d7f
Rimapus <58144397+Rimapus@users.noreply.github.com>
2025-12-02 17:59:44 +0000

    Credit custom CSS author

--- Misc/AoC style.user.js
+++ Misc/AoC style.user.js
@@ -2,38 +2,42 @@
 // @name         AoC style
 // @version      1
 // @description  Better AoC style
-// @author       Someone
+// @author       https://github.com/saidaspen
+// @source       https://gist.github.com/saidaspen/cbf216aa4be971a65b55426b884aee72
 // @match        https://adventofcode.com/*
+// @run-at       document-start
 // @grant        GM_addStyle
 
 // ==/UserScript==
 
-GM_addStyle("input ~ span:before,\
-.leaderboard-entry,\
-.privboard-row {\
-    font-family: 'Source Code Pro', monospace;\
-    letter-spacing: 1px;\
-}\
-\
-body * {\
-    text-shadow: none !important;\
-}\
-article em {\
-    font-weight: bold;\
-    color: #22ff68\
-}\
-\
-main > article + p,\
-main > article:not(:last-of-type) {\
-    opacity: 0.8;\
-}\
-\
-code {\
-    font-family: 'Source Code Pro', monospace;\
-    letter-spacing: 1px;\
-    color: #ff6e6e\
-}\
-\
-article code em {\
-    color: #ff6e6e\
-}")
+GM_addStyle(`
+input ~ span:before,
+.leaderboard-entry,
+.privboard-row {
+    font-family: "Source Code Pro", monospace;
+    letter-spacing: 1px;
+}
+
+body * {
+    text-shadow: none !important;
+}
+article em {
+    font-weight: bold;
+    color: #22ff68
+}
+
+main > article + p,
+main > article:not(:last-of-type) {
+    opacity: 0.8;
+}
+
+code {
+    font-family: "Source Code Pro", monospace;
+    letter-spacing: 1px;
+    color: #ff6e6e
+}
+
+article code em {
+    color: #ff6e6e
+}
+`);
